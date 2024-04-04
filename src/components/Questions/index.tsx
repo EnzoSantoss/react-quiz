9f386413182cfb91652262cead071e5f51cba6df
EnzoSantoss <enzoaugusto312@gmail.com>
2024-04-03 23:22:23 -0300

    adicionando perguntas de forma dinamica

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import "./Questions.modules.scss"
 
 //Hooks
+import React from 'react'
 import { useContext,useState } from 'react'
 
 
@@ -11,23 +12,36 @@ const Questions = () => {
 
   const [gameStage, dispatch] = useContext(QuizContext)
 
+  const [counter, setCounter] = useState(0)
 
-  const current_item = gameStage.questions[0]
+
+  const sumCounter = () =>{
+
+    counter < gameStage.questions.length - 1  ? setCounter( counter + 1) : setCounter(gameStage.questions.length-1)
+  }
+
+  const current_item = gameStage.questions[counter]
   
   console.log(gameStage)
   console.log(current_item)
 
+  console.log(counter)
+
   return (
     <div className='question-div'>
+
+      <div className="question-div-bg">
       <h2>{current_item.question}</h2> 
 
-      {<div className='options-div'>
-        <ul>
-          {current_item.options.map((e,index)=>{ return <li key={index}>{e}</li>}) }
-        </ul>
-      </div> }
+          {<div className='options-div'>
+            <ul>
+              {current_item.options.map((e,index)=>{ return <li key={index}>{e}</li>}) }
+            </ul>
+          </div> }
 
-      <button>Próxima pergunta</button>
+        <button onClick={sumCounter}>Próxima pergunta</button>
+      </div>
+      
     </div>
   )
 }
